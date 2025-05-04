86143e892ae6966f96bb4ad792f008553f2717ea
AlexeyTsekun2011 <cekunirina6@gmail.com>
2025-05-04 18:40:18 +1200

    New commit

--- pipe.py
+++ pipe.py
@@ -6,9 +6,9 @@ class Pipe(pg.sprite.Sprite):
         super(Pipe,self).__init__()
         spritesheet = pg.image.load("Flappy Bird Assets/Tiles/Style 1/PipeStyle1.png")
         image = spritesheet.subsurface((0,0,32,80))#32,80
-        self.image = pg.transform.scale_by(image, 3.7)
+        # self.image = pg.transform.scale_by(image, 3.7)
         self.image = pg.transform.scale(image, (110,350))
-        self.rect = image.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.x = SCREEN_WIDTH
         self.rect.y = y
 
